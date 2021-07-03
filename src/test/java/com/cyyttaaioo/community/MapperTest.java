bc20aeffabd66bc789fbec896f6dd3aaae7b9bdf
cyyttaaioo <fxyclown@foxmail.com>
2021-07-03 17:01:37 +0800

    消息列表

--- src/test/java/com/cyyttaaioo/community/MapperTest.java
+++ src/test/java/com/cyyttaaioo/community/MapperTest.java
@@ -2,9 +2,11 @@ package com.cyyttaaioo.community;
 
 import com.cyyttaaioo.community.dao.DiscussPostMapper;
 import com.cyyttaaioo.community.dao.LoginTicketMapper;
+import com.cyyttaaioo.community.dao.MessageMapper;
 import com.cyyttaaioo.community.dao.UserMapper;
 import com.cyyttaaioo.community.entity.DiscussPost;
 import com.cyyttaaioo.community.entity.LoginTicket;
+import com.cyyttaaioo.community.entity.Message;
 import com.cyyttaaioo.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,6 +31,9 @@ public class MapperTest {
 
     @Autowired
     private LoginTicketMapper loginTicketMapper;
+
+    @Autowired
+    private MessageMapper messageMapper;
 
     @Test
     public void testSelectUser(){
@@ -97,5 +102,28 @@ public class MapperTest {
         loginTicket = loginTicketMapper.selectByTicket("abc");
         System.out.println(loginTicket);
     }
+
+    @Test
+    public void testSelectLetters(){
+        List<Message> list = messageMapper.selectConversations(111, 0, 20);
+        for (Message message : list){
+            System.out.println(message);
+        }
+
+        int count = messageMapper.selectConversationCount(111);
+        System.out.println(count);
+
+        list = messageMapper.selectLetters("111_112", 0, 10);
+        for(Message message : list){
+            System.out.println(message);
+        }
+
+        count = messageMapper.selectLetterCount("111_112");
+        System.out.println(count);
+
+        count = messageMapper.selectLetterUnreadCount(131,"111_131");
+        System.out.println(count);
+    }
+
 
 }
