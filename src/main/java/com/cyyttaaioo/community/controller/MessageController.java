c263742ee1a5ff717291919085907d40b97ae43b
cyyttaaioo <fxyclown@foxmail.com>
2021-07-23 09:54:22 +0800

    权限控制(SpringSecurity)

--- src/main/java/com/cyyttaaioo/community/controller/MessageController.java
+++ src/main/java/com/cyyttaaioo/community/controller/MessageController.java
@@ -154,8 +154,9 @@ public class MessageController implements CommunityConstant {
 
         //查询评论类的通知
         Message message = messageService.findLatestNotice(user.getId(), TOPIC_COMMENT);
-        Map<String, Object> messageVo = new HashMap<>();
+
         if(message != null){
+            Map<String, Object> messageVo = new HashMap<>();
             messageVo.put("message", message);
 
             String content = HtmlUtils.htmlUnescape(message.getContent());
@@ -171,13 +172,15 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findNoticeUnreadCount(user.getId(), TOPIC_COMMENT);
             messageVo.put("unread", unread);
+            model.addAttribute("commentNotice", messageVo);
         }
-        model.addAttribute("commentNotice", messageVo);
+
 
         //查询点赞类的通知
         message = messageService.findLatestNotice(user.getId(), TOPIC_LIKE);
-        messageVo = new HashMap<>();
+
         if(message != null){
+            Map<String, Object> messageVo = new HashMap<>();
             messageVo.put("message", message);
 
             String content = HtmlUtils.htmlUnescape(message.getContent());
@@ -193,13 +196,15 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findNoticeUnreadCount(user.getId(), TOPIC_LIKE);
             messageVo.put("unread", unread);
+            model.addAttribute("likeNotice", messageVo);
         }
-        model.addAttribute("likeNotice", messageVo);
+
 
         //查询关注类的通知
         message = messageService.findLatestNotice(user.getId(), TOPIC_FOLLOW);
-        messageVo = new HashMap<>();
+
         if(message != null){
+            Map<String, Object> messageVo = new HashMap<>();
             messageVo.put("message", message);
 
             String content = HtmlUtils.htmlUnescape(message.getContent());
@@ -214,8 +219,9 @@ public class MessageController implements CommunityConstant {
 
             int unread = messageService.findNoticeUnreadCount(user.getId(), TOPIC_FOLLOW);
             messageVo.put("unread", unread);
+            model.addAttribute("followNotice", messageVo);
         }
-        model.addAttribute("followNotice", messageVo);
+
 
         //查询未读私信数量
         int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
