77cbaaf2be48faad3cef9af2bae96b1aadf61890
cyyttaaioo <fxyclown@foxmail.com>
2021-07-23 22:46:54 +0800

    线程池练习

--- src/main/java/com/cyyttaaioo/community/service/AlphaService.java
+++ src/main/java/com/cyyttaaioo/community/service/AlphaService.java
@@ -6,8 +6,11 @@ import com.cyyttaaioo.community.dao.UserMapper;
 import com.cyyttaaioo.community.entity.DiscussPost;
 import com.cyyttaaioo.community.entity.User;
 import com.cyyttaaioo.community.util.CommunityUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
@@ -24,6 +27,8 @@ import java.util.Date;
 @Service
 //@Scope("prototype")
 public class AlphaService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AlphaService.class);
 
     @Autowired
     private AlphaDao alphaDao;
@@ -112,6 +117,17 @@ public class AlphaService {
                 return "ok";
             }
         });
+    }
+
+    // 让该方法在多线程环境下被异步的调用
+    @Async
+    public void execute1(){
+        logger.debug("execute1");
+    }
+
+    //@Scheduled(initialDelay = 1000, fixedRate = 1000)
+    public void execute2(){
+        logger.debug("execute2");
     }
 
 }
